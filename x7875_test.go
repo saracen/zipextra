7347a2ee3f10ce128f2056726826f1b51673080a
Arran Walker <arran.walker@fiveturns.org>
2020-12-05 10:39:23 +0000

    x7875: set uid/gid minimum encode length to 4
    x7875 supports arbitrary sized uid/gid lengths, but some archivers assume a
    32-bit integer. For increased compatibility, at least 4 bytes will always be
    encoded, but any size can still be decoded.

--- x7875_test.go
+++ x7875_test.go
@@ -18,14 +18,15 @@ func TestInfoZIPNewUnix(t *testing.T) {
 		raw     string
 	}{
 		// normal ids
-		{1, big.NewInt(0), big.NewInt(0), "75780300010000"},
-		{1, big.NewInt(1), big.NewInt(1), "757805000101010101"},
-		{1, big.NewInt(255), big.NewInt(255), "757805000101ff01ff"},
-		{1, big.NewInt(1000), big.NewInt(1000), "757807000102e80302e803"},
+		{1, big.NewInt(0), big.NewInt(0), "75780b000104000000000400000000"},
+		{1, big.NewInt(1), big.NewInt(1), "75780b000104010000000401000000"},
+		{1, big.NewInt(255), big.NewInt(255), "75780b000104ff00000004ff000000"},
+		{1, big.NewInt(1000), big.NewInt(1000), "75780b000104e803000004e8030000"},
 
 		// unusual
 		{1, big.NewInt(math.MaxInt32), big.NewInt(math.MaxInt32), "75780b000104ffffff7f04ffffff7f"},
 		{1, big.NewInt(math.MaxInt64), big.NewInt(math.MaxInt64), "757813000108ffffffffffffff7f08ffffffffffffff7f"},
+		{1, new(big.Int).SetBytes([]byte{0x0a, 0x0b}), big.NewInt(math.MaxInt64), "75780f0001040b0a000008ffffffffffffff7f"},
 	}
 
 	for _, test := range tests {
@@ -37,5 +38,7 @@ func TestInfoZIPNewUnix(t *testing.T) {
 		unix, err := testHeader(t, raw, ExtraFieldUnixN).InfoZIPNewUnix()
 		require.NoError(t, err)
 		assert.Equal(t, test.raw, hex.EncodeToString(unix.Encode()))
+		assert.Equal(t, test.uid.String(), unix.Uid.String())
+		assert.Equal(t, test.gid.String(), unix.Gid.String())
 	}
 }
